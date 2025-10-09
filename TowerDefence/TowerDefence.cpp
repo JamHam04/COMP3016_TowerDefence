b87fe9ccadd341c8c546c681bf88ea5eae5b064a
James Hampson <james.hampson2004@gmail.com>
2025-10-09 19:02:42 +0100

    Creating Path
    Adds CreatePath function to draw in the path in which enemies will move through. This still needs functionality to draw the path when it moves left or right

--- TowerDefence/TowerDefence.cpp
+++ TowerDefence/TowerDefence.cpp
@@ -5,11 +5,45 @@
 using namespace std;
 
 bool gameOver = false;
-const int width = 40;
-const int height = 20;
+const int width = 25;
+const int height = 30;
+
+// Path coordinates
+int pathX[100], pathY[100];
+int pathLength = 0;
+
+void createPath() {
+	int currentCol = 10; // Where path will start 
+	int currentPath = 0;
+	// Loop throuugh rows
+	for (int i = 0; i < height; i++) {
+		pathX[currentPath] = currentCol; // Set X to current col position
+		pathY[currentPath] = i; // Set Y to current row
+		currentPath++; // Increment array index
+
+		// Move left or right
+		if (i == 5) {
+			currentCol += 3; // Move 3right
+		}
+
+		if (i == 12) {
+			currentCol -= 7;
+		}
+
+		if (i == 17) {
+			currentCol -= 4;
+		}
+
+		if (i == 20) {
+			currentCol += 10;
+		}
+	}
+	pathLength = currentPath; //Stop path when it reaches end
+}
 
 void Setup() {
 	// Set Variables
+	createPath();
 }
 
 void Draw() {
@@ -26,8 +60,20 @@ void Draw() {
 		for (int j = 0; j < width; j++) { // Columns
 			if (j == 0) cout << '#'; // Print left border
 
-
-		    cout << ' ';
+			// Draw path
+			bool Path = false;
+			// Loop through map
+			for (int k = 0; k < pathLength; k++) {
+				if (pathX[k] == j && pathY[k] == i) { // If position is a path
+					Path = true; // Set position as path
+					break;
+				}
+			}
+			if (Path) cout << 'p'; // print on path position
+			// Else empty space
+			else {
+				cout << ' ';
+			}
 
 			if (j == width - 1) cout << '#'; // Print right border
 
