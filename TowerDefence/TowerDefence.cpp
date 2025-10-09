e37e76dea329392025a8741a7a102d0c44c20603
James Hampson <james.hampson2004@gmail.com>
2025-10-09 15:50:37 +0100

    Draw Map Outline
    Create the map to be used for the game

--- TowerDefence/TowerDefence.cpp
+++ TowerDefence/TowerDefence.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 bool gameOver = false;
+const int width = 40;
+const int height = 20;
 
 void Setup() {
 	// Set Variables
@@ -12,6 +14,32 @@ void Setup() {
 
 void Draw() {
 	// Draw Map
+	system("cls");
+	// Draw top border
+	for (int i = 0; i < width + 2; i++) { //Print one row along the top width (+2 for corners)
+		cout << '#';
+	}
+	cout << endl;
+
+	// Centre Map
+	for (int i = 0; i < height; i++) { // Rows
+		for (int j = 0; j < width; j++) { // Columns
+			if (j == 0) cout << '#'; // Print left border
+
+
+		    cout << ' ';
+
+			if (j == width - 1) cout << '#'; // Print right border
+
+		}
+		cout << endl; //Move to next row
+	}
+
+	// Draw bottom border
+	for (int i = 0; i < width + 2; i++) { //Print one along the bottom width (+2 for corners)
+		cout << '#';
+	}
+	cout << endl;
 
 	// Draw Towers
 
